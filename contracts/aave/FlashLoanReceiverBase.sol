311ae3bc2f732416d4c364dae60f8d9898faa65e
Zero41 <34267956+Zer041@users.noreply.github.com>
2023-07-30 03:45:45 +0200

    Update FlashLoanReceiverBase.sol

--- contracts/aave/FlashLoanReceiverBase.sol
+++ contracts/aave/FlashLoanReceiverBase.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.6.6;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.3/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./IFlashLoanReceiver.sol";
